9c6da16734b33bbe9bf9b0caa6100072e680b1a1
jclencioni <juancruzlencioni@gmail.com>
2026-05-22 20:47:43 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+
+function $(id){
+    return document.getElementById(id);
+}
+
+const contenedorCartas = $("cartas")
+    
+
+async function crearCartas(listaCartas){
+    
+    listaCartas.forEach(element => {
+
+        var carta = Carta.createFromJsonString(JSON.stringify(element));
+        var cartaCreada = carta.createHTMLElement(); 
+        contenedorCartas.appendChild(cartaCreada);
+    });
+}
+
+async function cargarTandaCartas() {
+    const urlBase = "https://deckofcardsapi.com/api/deck/new/draw/?count=6";
+
+    try {
+        var respuesta = await fetch(urlBase);
+        
+        if (!respuesta.ok) {
+            alert("Carta no encontrada");
+        } else {
+            var rta = await respuesta.json(); 
+            var cartas = rta.cards;  
+            console.log(cartas);
+            await crearCartas(cartas);
+
+        }
+    } catch (error) {
+        alert("Se perdió la conexion.")
+    }
+}
+
+async function PaginaSiguiente(){
+    contenedorCartas.innerHTML = "";
+    await cargarTandaCartas();
+}
+
+async function PaginaAnterior(){
+    contenedorCartas.innerHTML = "";
+    await cargarTandaCartas();
+}
+
+$("siguiente").addEventListener("click", PaginaSiguiente);
+$("anterior").addEventListener("click", PaginaAnterior);
+
+cargarTandaCartas();
